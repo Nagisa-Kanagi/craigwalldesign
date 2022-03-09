4bdaa68b62829a85321b641f3f9e0132f8db14a4
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-03-08 18:21:01 -0700

    Update script-preston.js

--- Weather/script-preston.js
+++ Weather/script-preston.js
@@ -46,3 +46,14 @@ const date2 = document.querySelector('#currentdate2');
 // } catch (e) {
 //   alert('Error with code or your browser does not support Locale');
 // }
+
+if (dayName == "Friday") {
+	var banner = document.createElement("div");
+	banner.className = "b";
+	banner.innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.fontSize = "x-large";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.textAlign = "center";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.padding = "20px 20px 20px 20px";
+	document.body.insertBefore(banner,document.body.childNodes[0]).style.backgroundColor = "#f5cf87";
+}
