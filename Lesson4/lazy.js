01853031c0f49a205e7dbbf70c04e2328f345dc1
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-03-14 21:01:20 -0600

    Add files via upload

---
+++ Lesson4/lazy.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll('img[data-src]');
+
+const imageOptions = {
+    threshold: 1.0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {image.removeAttribute('data-src');};
+};
+
+if('IntersectionObserver' in window) {
+    const imgobserver = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          imgobserver.unobserve(item.target);
+        }
+      });
+    });
+    images.forEach((img) => {
+        imgobserver.observe(img);
+    });
+  } 
+
+else {
+    images.forEach((img) => {
+      loadImages(img);
+    });
+  }
