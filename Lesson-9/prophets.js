e8365df13dc256d2c49d4f0ecf8fb7d712bbea5e
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-03-21 18:51:52 -0600

    Update prophets.js

--- Lesson-9/prophets.js
+++ Lesson-9/prophets.js
@@ -6,27 +6,23 @@ fetch(requestURL)
       })
 
       .then(function (jsonObject) {
-        console.table(jsonObject);
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
         const prophets = jsonObject['prophets'];
         for (let i = 0; i < prophets.length; i++ ) {
-          let card = document.createElement('section');
-          let h2 = document.createElement('h2');
-          h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-          card.appendChild(h2);
-          document.querySelector('div.cards').appendChild(card);
-          let h3 = document.createElement('h3');
-          h3.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-          card.appendChild(h3);
-          let h4 = document.createElement('h4');
-          h4.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-          card.appendChild(h4);
-          let img = document.createElement('img');
-          img.setAttribute('src', prophets[i].imageurl);
-          img.setAttribute('alt', prophets[i].name +' '+ prophets[i].lastname + ' -' + i+1);
-          card.appendChild(img);
-
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            card.appendChild(h2);
+            let h3 = document.createElement('h3');
+            h3.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            card.appendChild(h3);
+            let h4 = document.createElement('h4');
+            h4.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+            card.appendChild(h4);
+            let img = document.createElement('img');
+            img.setAttribute('src', prophets[i].imageurl);
+            img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + i+1);
+            card.appendChild(img);
+            document.querySelector('div.cards').appendChild(card);
         }
-          
-   
-          
-        });
+      });
