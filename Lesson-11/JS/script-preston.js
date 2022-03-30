6e90cc8540f0cf81a2af4a7981fcfedc5d239ec1
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-03-30 17:56:20 -0600

    Update script-preston.js

--- Lesson-11/JS/script-preston.js
+++ Lesson-11/JS/script-preston.js
@@ -78,3 +78,43 @@ if('IntersectionObserver' in window) {
       loadImages(img);
     });
   }
+
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject["towns"].filter((town) => {
+      if (town.name === "Preston") {
+        return town;
+      }
+    });
+    for (let i = 0; i < towns.length; i++) {
+      let events = document.createElement("section");
+
+      let headline = document.createElement("h2");
+      headline.textContent = "Upcoming Events";
+      events.appendChild(headline);
+
+      let event1 = document.createElement("h4");
+      event1.textContent = towns[i].events[0];
+      events.appendChild(event1);
+
+      let event2 = document.createElement("h4");
+      event2.textContent = towns[i].events[1];
+      events.appendChild(event2);
+
+      let event3 = document.createElement("h4");
+      event3.textContent = towns[i].events[2];
+      events.appendChild(event3);
+
+      let event4 = document.createElement("h4");
+      event4.textContent = towns[i].events[3];
+      events.appendChild(event4);
+
+      document.querySelector("div.events").appendChild(events);
+    }
+  });
