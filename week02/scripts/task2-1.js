bcc594452f6fce7f7adae1781d3f093bb7ba5590
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-03-30 22:02:44 -0600

    Add files via upload

---
+++ week02/scripts/task2-1.js
@@ -0,0 +1,37 @@
+const requestURL = "https://swapi.dev/api/people";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const results = jsonObject["results"].filter((result) => {
+      if (result.name === "Luke Skywalker") {
+        return result;
+      }
+    });
+    for (let i = 0; i < results.length; i++) {
+      let events = document.createElement("section");
+
+      let event1 = document.createElement("h4");
+      event1.textContent = results[i].height;
+      events.appendChild(event1);
+
+      let event2 = document.createElement("h4");
+      event2.textContent = results[i].hair_color;
+      events.appendChild(event2);
+
+      let event3 = document.createElement("h4");
+      event3.textContent = results[i].eye_color;
+      events.appendChild(event3);
+
+      let event4 = document.createElement("h4");
+      event4.textContent = results[i].gender;
+      events.appendChild(event4);
+
+      document.querySelector("div.events").appendChild(events);
+    }
+  });
+
+const year = 2022;
+document.querySelector('#year').textContent = year;
