a4945718a235ecfef003c9252cc5ba7cc717e32b
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-03-31 17:17:20 -0600

    Update task2-1.js

--- week02/scripts/task2-1.js
+++ week02/scripts/task2-1.js
@@ -6,28 +6,30 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const results = jsonObject["results"].filter((result) => {
-      if (result.name === "Luke Skywalker") {
         return result;
-      }
-    });
+  });
     for (let i = 0; i < results.length; i++) {
       let events = document.createElement("section");
 
-      let event1 = document.createElement("h4");
-      event1.textContent = results[i].height;
-      events.appendChild(event1);
+      let event1 = document.createElement("h4")
+      event1.textContent = results[i].name;
+      events.appendChild(event1)
 
       let event2 = document.createElement("h4");
-      event2.textContent = results[i].hair_color;
+      event2.textContent = results[i].height;
       events.appendChild(event2);
 
       let event3 = document.createElement("h4");
-      event3.textContent = results[i].eye_color;
+      event3.textContent = results[i].hair_color;
       events.appendChild(event3);
 
       let event4 = document.createElement("h4");
-      event4.textContent = results[i].gender;
+      event4.textContent = results[i].eye_color;
       events.appendChild(event4);
+
+      let event5 = document.createElement("h4");
+      event5.textContent = results[i].gender;
+      events.appendChild(event5);
 
       document.querySelector("div.events").appendChild(events);
     }
