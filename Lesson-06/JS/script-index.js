c035c15902b383b7618a021463a1763daa78c21c
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-04-01 21:55:49 -0600

    Update script-index.js

--- Lesson-06/JS/script-index.js
+++ Lesson-06/JS/script-index.js
@@ -7,30 +7,31 @@ fetch(requestURL)
         console.table(jsonObject); const towns = jsonObject['towns'];
         for(let t=0; t<towns.length; t++) {
             let card = document.createElement('section');
-            card.classList.add('disney');
+            
             let h2 =document.createElement('h2');
             h2.textContent = towns[t].name;
+            card.appendChild(h2);
+            
             let averageRainfall = document.createElement('p');
             averageRainfall.textContent = "Average Rainfall:" + ' ' + towns[t].averageRainfall;
+            card.append(averageRainfall);
+            
             let motto = document.createElement('p');
             motto.textContent = towns[t].motto;
+            card.appendChild(motto);
+            
             let events = document.createElement('p');
             events.textContent = "Events:" + ' ' + towns[t].events;
+            card.append(events);
+            
             let yearFounded = document.createElement('p');
             yearFounded.textContent = "Year Founded:" + ' ' + towns[t].yearFounded;
+            card.appendChild(yearFounded);
+            
             let currentPopulation = document.createElement('p');
             currentPopulation = "Current Population:" + ' ' + towns[t].currentPopulation;
-            let photo = document.createElement('img');
-            photo.setAttribute('src', `../Weather/images/${towns[t].photo}`);
-            photo.classList.add('idaho');
-            photo.setAttribute('alt', towns[t].name);
-            card.appendChild(h2);
-            card.appendChild(motto);
-            card.appendChild(yearFounded);
-            card.append(averageRainfall);
             card.append(currentPopulation);
-            card.append(events);
-            card.append(photo);
-            document.querySelector('div.upperstclair').appendChild(card);
+            
+            document.querySelector('div.data').appendChild(card);
         }
     })
