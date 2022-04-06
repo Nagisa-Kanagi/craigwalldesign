e148565dba0838b62b78457bc0518bb1c5cc943d
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-04-06 09:29:19 -0600

    Update and rename script-main.js to script-final.js

---
+++ WDD-230 Final/script-final.js
@@ -0,0 +1,50 @@
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+const daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
+document.getElementById("currentdate").textContent = fulldate;
+
+const date2 = document.querySelector("#currentdate2");
