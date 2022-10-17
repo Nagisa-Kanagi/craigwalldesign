15680de9fb3afdb6aa3dd9c0f3bc49ab5d4b6860
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-10-17 13:56:02 -0600

    Update weatherapi.js

--- Chamber/JS/weatherapi.js
+++ Chamber/JS/weatherapi.js
@@ -1,4 +1,3 @@
-//OpenWeatherMap: API Key (AppId) - 3269efc82e03c378edf3680c194b11a1
 const weatherApiURL =
   "https://api.openweathermap.org/data/2.5/weather?id=5780993&units=imperial&appid=b7459806cc0deb3409dbe6c09533b316";
 
@@ -7,7 +6,6 @@ fetch(weatherApiURL)
     return response.json();
   })
   .then(function (jsonObject) {
-
     if (document.querySelector("#temperature") !== null) {
       document.querySelector("#temperature").textContent = jsonObject.main.temp;
     }
