b09424b8e0d7d66679216b68d1f2824ac843cf6f
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-10-24 18:13:12 -0600

    Update script.js

--- Chamber/JS/script.js
+++ Chamber/JS/script.js
@@ -86,3 +86,29 @@ const mediaQuery2 = window.matchMedia("(min-width:33em, max-width: 55em)");
 if (mediaQuery2.matches) {
   document.getElementById("overlay").innerHTML = "Medium";
 }
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
