da1c3fa4b3786d44400c3127a814773ab0ade239
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-11-13 19:56:32 -0700

    Update weatherapi.js

--- Lesson-10/JS/weatherapi.js
+++ Lesson-10/JS/weatherapi.js
@@ -1,4 +1,3 @@
-//OpenWeatherMap: API Key (AppId) - 3269efc82e03c378edf3680c194b11a1
 const weatherApiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=b7459806cc0deb3409dbe6c09533b316";
 const forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=b7459806cc0deb3409dbe6c09533b316";
 
