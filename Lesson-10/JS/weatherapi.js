75d8368fd847b1acb025085b20fe70dfe3ac4986
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-11-13 20:33:03 -0700

    Update weatherapi.js

--- Lesson-10/JS/weatherapi.js
+++ Lesson-10/JS/weatherapi.js
@@ -1,8 +1,5 @@
 const weatherApiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=b7459806cc0deb3409dbe6c09533b316";
 const forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=b7459806cc0deb3409dbe6c09533b316";
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
 
 fetch(weatherApiURL)
   .then(function (response) {
@@ -18,5 +15,6 @@ fetch(weatherApiURL)
 		document.querySelector("#weather-icon").textContent = imagesrc;
 		document.querySelector("#weather-icon").setAttribute("src", imagesrc);
 		document.querySelector("#weather-icon").setAttribute("alt", desc);
+		document.querySelector("figcaption").textContent = desc;
 	}
   });
