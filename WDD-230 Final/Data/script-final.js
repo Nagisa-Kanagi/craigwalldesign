b697a928fff04b0a14cf78edf76199bcb4440c76
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2022-12-02 00:11:37 -0700

    Update script-final.js

--- WDD-230 Final/Data/script-final.js
+++ WDD-230 Final/Data/script-final.js
@@ -13,6 +13,11 @@ window.onresize = () => {
   if (window.innerWidth > 760) mainnav.classList.remove("responsive");
 };
 
+document.querySelector(
+  "#lastModified"
+).textContent = `Last Modified: ${document.lastModified}`;
+
+
 const daynames = [
   "Sunday",
   "Monday",
@@ -22,7 +27,6 @@ const daynames = [
   "Friday",
   "Saturday",
 ];
-
 const months = [
   "January",
   "February",
@@ -37,7 +41,6 @@ const months = [
   "November",
   "December",
 ];
-
 const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
@@ -48,3 +51,35 @@ const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 document.getElementById("currentdate").textContent = fulldate;
 
 const date2 = document.querySelector("#currentdate2");
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+var n = new Date();
+var hours = n.getHours();
+var mins = n.getMinutes();
+var seconds = n.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
