1977c9528baa1f619883d139432d8215520614c4
Nagisa-Kanagi <97491572+Nagisa-Kanagi@users.noreply.github.com>
2023-04-22 21:16:34 -0600

    Create stock.js

---
+++ craigwalldesign/stock.js
@@ -0,0 +1,280 @@
+var modal = document.getElementById("Modal");
+
+var plan = document.getElementById("Plan1");
+plan.onclick = function(){
+  modal.style.display = "grid";
+  modal.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+var modal2 = document.getElementById("Modal2");
+
+var plan2 = document.getElementById("Plan2");
+plan2.onclick = function(){
+  modal2.style.display = "grid";
+  modal2.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+span2.onclick = function() { 
+  modal2.style.display = "none";
+}
+
+var modal3 = document.getElementById("Modal3");
+
+var plan3 = document.getElementById("Plan3");
+plan3.onclick = function(){
+  modal3.style.display = "grid";
+  modal3.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span3 = document.getElementsByClassName("close3")[0];
+
+span3.onclick = function() { 
+  modal3.style.display = "none";
+}
+
+var modal4 = document.getElementById("Modal4");
+
+var plan4 = document.getElementById("Plan4");
+plan4.onclick = function(){
+  modal4.style.display = "grid";
+  modal4.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span4 = document.getElementsByClassName("close4")[0];
+
+span4.onclick = function() { 
+  modal4.style.display = "none";
+}
+
+var modal5 = document.getElementById("Modal5");
+
+var plan5 = document.getElementById("Plan5");
+plan5.onclick = function(){
+  modal5.style.display = "grid";
+  modal5.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span5 = document.getElementsByClassName("close5")[0];
+
+span5.onclick = function() { 
+  modal5.style.display = "none";
+}
+
+var modal6 = document.getElementById("Modal6");
+
+var plan6 = document.getElementById("Plan6");
+plan6.onclick = function(){
+  modal6.style.display = "grid";
+  modal6.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span6 = document.getElementsByClassName("close6")[0];
+
+span6.onclick = function() { 
+  modal6.style.display = "none";
+}
+
+var modal7 = document.getElementById("Modal7");
+
+var plan7 = document.getElementById("Plan7");
+plan7.onclick = function(){
+  modal7.style.display = "grid";
+  modal7.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span7 = document.getElementsByClassName("close7")[0];
+
+span7.onclick = function() { 
+  modal7.style.display = "none";
+}
+
+var modal8 = document.getElementById("Modal8");
+
+var plan8 = document.getElementById("Plan8");
+plan8.onclick = function(){
+  modal8.style.display = "grid";
+  modal8.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span8 = document.getElementsByClassName("close8")[0];
+
+span8.onclick = function() { 
+  modal8.style.display = "none";
+}
+
+var modal9 = document.getElementById("Modal9");
+
+var plan9 = document.getElementById("Plan9");
+plan9.onclick = function(){
+  modal9.style.display = "grid";
+  modal9.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span9 = document.getElementsByClassName("close9")[0];
+
+span9.onclick = function() { 
+  modal9.style.display = "none";
+}
+
+var modal10 = document.getElementById("Modal10");
+
+var plan10 = document.getElementById("Plan10");
+plan10.onclick = function(){
+  modal10.style.display = "grid";
+  modal10.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span10 = document.getElementsByClassName("close10")[0];
+
+span10.onclick = function() { 
+  modal10.style.display = "none";
+}
+
+var modal11 = document.getElementById("Modal11");
+
+var plan11 = document.getElementById("Plan11");
+plan11.onclick = function(){
+  modal11.style.display = "grid";
+  modal11.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span11 = document.getElementsByClassName("close11")[0];
+
+span11.onclick = function() { 
+  modal11.style.display = "none";
+}
+
+var modal12 = document.getElementById("Modal12");
+
+var plan12 = document.getElementById("Plan12");
+plan12.onclick = function(){
+  modal12.style.display = "grid";
+  modal12.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span12 = document.getElementsByClassName("close12")[0];
+
+span12.onclick = function() { 
+  modal12.style.display = "none";
+}
+
+var modal13 = document.getElementById("Modal13");
+
+var plan13 = document.getElementById("Plan13");
+plan13.onclick = function(){
+  modal13.style.display = "grid";
+  modal13.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span13 = document.getElementsByClassName("close13")[0];
+
+span13.onclick = function() { 
+  modal13.style.display = "none";
+}
+
+var modal14 = document.getElementById("Modal14");
+
+var plan14 = document.getElementById("Plan14");
+plan14.onclick = function(){
+  modal14.style.display = "grid";
+  modal14.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span14 = document.getElementsByClassName("close14")[0];
+
+span14.onclick = function() { 
+  modal14.style.display = "none";
+}
+
+var modal15 = document.getElementById("Modal15");
+
+var plan15 = document.getElementById("Plan15");
+plan15.onclick = function(){
+  modal15.style.display = "grid";
+  modal15.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span15 = document.getElementsByClassName("close15")[0];
+
+span15.onclick = function() { 
+  modal15.style.display = "none";
+}
+
+var modal16 = document.getElementById("Modal16");
+
+var plan16 = document.getElementById("Plan16");
+plan16.onclick = function(){
+  modal16.style.display = "grid";
+  modal16.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span16 = document.getElementsByClassName("close16")[0];
+
+span16.onclick = function() { 
+  modal16.style.display = "none";
+}
+
+var modal17 = document.getElementById("Modal17");
+
+var plan17 = document.getElementById("Plan17");
+plan17.onclick = function(){
+  modal17.style.display = "grid";
+  modal17.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span17 = document.getElementsByClassName("close17")[0];
+
+span17.onclick = function() { 
+  modal17.style.display = "none";
+}
+
+var modal18 = document.getElementById("Modal18");
+
+var plan18 = document.getElementById("Plan18");
+plan18.onclick = function(){
+  modal18.style.display = "grid";
+  modal18.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span18 = document.getElementsByClassName("close18")[0];
+
+span18.onclick = function() { 
+  modal18.style.display = "none";
+}
+
+var modal19 = document.getElementById("Modal19");
+
+var plan19 = document.getElementById("Plan19");
+plan19.onclick = function(){
+  modal19.style.display = "grid";
+  modal19.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span19 = document.getElementsByClassName("close19")[0];
+
+span19.onclick = function() { 
+  modal19.style.display = "none";
+}
+
+var modal20 = document.getElementById("Modal20");
+
+var plan20 = document.getElementById("Plan20");
+plan20.onclick = function(){
+  modal20.style.display = "grid";
+  modal20.style.gridTemplateColumns = "1fr 1fr";
+}
+
+var span20 = document.getElementsByClassName("close20")[0];
+
+span20.onclick = function() { 
+  modal20.style.display = "none";
+}
